1258b775991afd6be470b4f56f0b88de4d2c0555
FadiChamas <thefadichamas@gmail.com>
2022-04-12 00:24:14 -0400

    Completed tictactoe buttons logic

--- src/main/java/project/tictactoe/ClientOController.java
+++ src/main/java/project/tictactoe/ClientOController.java
@@ -1,4 +1,29 @@
 package project.tictactoe;
 
-public class ClientOController {
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+import static java.lang.String.valueOf;
+
+public class ClientOController{
+    @FXML
+    GridPane gridPane;
+
+    public String getString(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        ObservableList<Node> kids = gridPane.getChildren();
+        for (Node kid : kids) {
+            if (kid == node) {
+                System.out.println("O " + gridPane.getRowIndex(kid)  + " " + gridPane.getColumnIndex(kid));
+                return "O " + gridPane.getRowIndex(kid) + " " + gridPane.getColumnIndex(kid);
+            }
+        }
+        return "error";
+    }
 }
--- src/main/java/project/tictactoe/ClientXController.java
+++ src/main/java/project/tictactoe/ClientXController.java
@@ -1,4 +1,25 @@
 package project.tictactoe;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+
 public class ClientXController {
+    @FXML
+    GridPane gridPain;
+    public String getString(ActionEvent event) {
+
+
+        Node node = (Node) event.getSource();
+        ObservableList<Node> kids = gridPain.getChildren();
+        for (Node kid : kids) {
+            if (kid == node) {
+                System.out.println("X " + gridPain.getRowIndex(kid)  + " " + gridPain.getColumnIndex(kid));
+                return "X " + gridPain.getRowIndex(kid) + " " + gridPain.getColumnIndex(kid);
+            }
+        }
+        return "error";
+    }
 }
